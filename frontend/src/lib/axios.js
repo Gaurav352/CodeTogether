d02895d422357f3b68b367e7fb1ad23bdf6d443a
Gaurav352 <gauravy81039@gmail.com>
2026-05-13 21:01:25 +0530

    configured env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-
-const baseURL = import.meta.env.mode === "development" ? "http://localhost:5000/api" :"/";
 const axiosInstance = axios.create({
-  baseURL: baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
